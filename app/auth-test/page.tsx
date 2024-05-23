4e1b3b63e9ea7dcabf7df268f03c4b3f1ae83099
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-05-23 23:25:45 +0545

    changes

--- app/auth-test/page.tsx
+++ app/auth-test/page.tsx
@@ -4,19 +4,19 @@ import { supabase } from "@/utils/supabase/supabaseClient";
 import React from "react";
 
 export default function page() {
-  const handleCreate = async () => {
-    const { data, error } = await supabase.auth.admin.createUser({
-      email: "lokichaulagain4@gmail.com",
-      password: "Password",
-      email_confirm: true,
-      user_metadata: {
-        // full_name: values.full_name,
-        // username: values.username,
-        // role: values.role,
-      },
-      role: "something"   // only predefined roles can be assigned
-    });
-  };
+  // const handleCreate = async () => {
+  //   const { data, error } = await supabase.auth.admin.createUser({
+  //     email: "lokichaulagain4@gmail.com",
+  //     password: "Password",
+  //     email_confirm: true,
+  //     user_metadata: {
+  //       // full_name: values.full_name,
+  //       // username: values.username,
+  //       // role: values.role,
+  //     },
+  //     role: "something"   // only predefined roles can be assigned
+  //   });
+  // };
 
   // const handleCreate = async () => {
   //   const { data, error } = await supabase.auth.signUp({
@@ -31,9 +31,20 @@ export default function page() {
   //   });
   // };
 
+  const signUpNewVendor = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email: "vendor4@gmail.com",
+      password: "Password",
+      options: {
+        data: { role: "vendor" },
+        emailRedirectTo: "https://example.com/welcome",
+      },
+    });
+  };
+
   return (
     <div>
-      <Button onClick={handleCreate}>Create User</Button>
+      <Button onClick={signUpNewVendor}>Create User</Button>
     </div>
   );
 }
