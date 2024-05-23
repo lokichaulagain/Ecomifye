47b5d7b5baf906c6c9e3216af9a344e4f6c040a4
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-05-23 15:39:03 +0545

    push

--- app/auth-test/page.tsx
+++ app/auth-test/page.tsx
@@ -1,10 +1,39 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { google } from "../actions/auth";
+import { supabase } from "@/utils/supabase/supabaseClient";
+import React from "react";
 
-export default function Page() {
+export default function page() {
+  const handleCreate = async () => {
+    const { data, error } = await supabase.auth.admin.createUser({
+      email: "lokichaulagain4@gmail.com",
+      password: "Password",
+      email_confirm: true,
+      user_metadata: {
+        // full_name: values.full_name,
+        // username: values.username,
+        // role: values.role,
+      },
+      role: "something"   // only predefined roles can be assigned
+    });
+  };
+
+  // const handleCreate = async () => {
+  //   const { data, error } = await supabase.auth.signUp({
+  //     email: "lokichaulagain2@gmail.com",
+  //     password: "Password",
+  //     options: {
+  //       data: {
+  //         role: "vendor",
+  //         email: "lokichaulagain2@gmail.com",
+  //       },
+  //     },
+  //   });
+  // };
+
   return (
-    <form>
-      <Button formAction={google}>Sign up</Button>
-    </form>
+    <div>
+      <Button onClick={handleCreate}>Create User</Button>
+    </div>
   );
 }
